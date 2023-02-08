7eb7e7c8673203ec2d01ef03037ee05441f6cff6
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-08 19:50:07 +0100

    FEAT: fine esercizio 2 pari e dispari

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,6 @@
 console.log("Ciao Cianuro.. oggi PALI & DISPARI");
-
+// .....................................................................................................ESERCIZIO PAROLA PALINDROMA
+// .....................................................................................................
 // CREO INPUT PER RICHIEDERE PAROLA
 let parola = prompt("Inserisci una parola");
 console.log(parola);
@@ -14,7 +15,51 @@ if (risulto === true) {
   document.write("la parola " + parola + " NON è palindroma");
 }
 
-// INIZIALIZZO LA FUNZIONE
+// .....................................................................................................ESERCIZIO PARI
+// .....................................................................................................
+// CREO INPUT PER RICHIEDERE ALL'UTENTE SE VUOLE PARI O DISPARI E FACCIO LA VERIFICA
+let userChoice = prompt("Preferisci pari o dispari?");
+console.log(userChoice);
+while (userChoice !== "pari" && userChoice !== "dispari") {
+  alert(
+    "Cortesemente inserisci la tua scelta digitando tutto in minuscolo e scrivendo solo pari o dispari"
+  );
+  userChoice = prompt("Preferisci pari o dispari?");
+  console.log(userChoice);
+}
+// CREO INPUT PER RICHIEDERE ALL'UTENTE UN NUMERO DA 1 A 5E FACCIO LA VERIFICA
+let userNumber = prompt("Inserisci un numero compreso tra 1 e 5");
+console.log(userNumber);
+while (userNumber < 1 || userNumber > 5) {
+  alert("Inserisci un numero compreso tra 1 e 5");
+  userNumber = prompt("Inserisci un numero compreso tra 1 e 5");
+  console.log(userNumber);
+}
+// GENERO NUMERO RANDOM TRA 1 E 5 PER PC
+let pcNumber = random1To5();
+console.log(pcNumber);
+
+// SOMMO NUMERO UTENTE E NUMERO PC
+let sum = parseInt(userNumber) + parseInt(pcNumber);
+console.log(sum);
+
+// STABILISCO IL VINCITORE
+let result = pariDispari();
+console.log(result);
+if (result === true) {
+  document.write(
+    "<br>" + "Complimenti... Hai vinto... è uscito un numero pari"
+  );
+} else {
+  document.write(
+    "<br>" + "Mi dispiace... Hai perso... è uscito un numero dispari"
+  );
+}
+
+// .....................................................................................................INIZIALIZZO LE FUNZIONI
+// .....................................................................................................
+
+// INIZIALIZZO LA FUNZIONE PER LA PAROLA PALINDROMA
 function palindroma(argomento) {
   // PRENDO LA PAROLA E LA METTO IN UN ARRAY DIVIDENDOLA IN LETTERE
   let putInArray = argomento.split("");
@@ -33,6 +78,21 @@ function palindroma(argomento) {
 
   // CONFRONTO LE PAROLE
   if (parola1 === parola2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// INIZIALIZZO LA FUNZIONE NUMERO RANDOM DA 1 A 5
+function random1To5() {
+  let randomNumber = Math.floor(Math.random() * 5) + 1;
+  //   console.log(randomNumber);
+  return randomNumber;
+}
+// INIZIALIZZO LA FUNZIONE PARI O DISPARI
+function pariDispari() {
+  if (sum % 2 === 0) {
     return true;
   } else {
     return false;
